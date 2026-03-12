85825b600bc442ca741b02156c1f34cc49f3c675
Akanksha7Dixit <akadixit98@gmail.com>
2026-03-13 00:35:57 +0530

    ready for render deployment

--- backend/server.js
+++ backend/server.js
@@ -84,3 +84,11 @@ mongoose
     console.error('[DB] Connection failed:', err.message)
     process.exit(1)
   })
+
+  const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
